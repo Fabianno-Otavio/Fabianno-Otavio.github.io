c41e7230a57da266561d28b0519c6ae7160db421
Fabianno Otávio <fabiannootavio08@gmail.com>
2022-08-08 16:02:56 -0300

    Fix: repositioned button on abt

--- js/components/main-form.js
+++ js/components/main-form.js
@@ -1,9 +1,8 @@
 function validateForm(){
-    event.preventDefault();
+
     let name = document.forms[0].name;
     let mail = document.forms[0].email;    
     let msg = document.forms[0].message;
-    let btn = document.forms[0].send;
     
     name.value == '' ? window.alert('Coloque seu nome.') :
     mail.value == '' ? window.alert('Coloque seu e-mail.') :
